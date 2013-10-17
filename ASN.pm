3cb793137770d7512dbbe3f78c654394922df4ce
Wes Hardaker <wjhns117@hardakers.net>
2013-10-17 14:34:25 +0000

    Import of HARDAKER/NetSNMP-ASN-5.0404 from CPAN.
    gitpan-cpan-distribution: NetSNMP-ASN
    gitpan-cpan-version:      5.0404
    gitpan-cpan-path:         HARDAKER/NetSNMP-ASN-5.0404.tar.gz
    gitpan-cpan-author:       HARDAKER
    gitpan-cpan-maturity:     released

--- ASN.pm
+++ ASN.pm
@@ -1,6 +1,7 @@
 package NetSNMP::ASN;
 
 use strict;
+use warnings;
 use Carp;
 
 require Exporter;
@@ -65,7 +66,7 @@ use vars qw(@ISA %EXPORT_TAGS @EXPORT_OK @EXPORT $VERSION $AUTOLOAD);
 	ASN_UNSIGNED
 	ASN_UNSIGNED64
 );
-$VERSION = '5.0401';
+$VERSION = '5.0404';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
